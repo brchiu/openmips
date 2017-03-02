00348cd11647882e662ca923f3a16e19b8296a3b
Bi-Ruei, Chiu <biruei.chiu@gmail.com>
2017-03-03 02:36:49 +0800

    Fix typo in regfile.v of chapter4

--- chapter4/regfile.v
+++ chapter4/regfile.v
@@ -50,11 +50,11 @@ module regfile(
     always @(*) begin
         if (rst == `RstEnable) begin
             rdata2 <= `ZeroWord;
-        end else if (raddr1 == `RegNumLog2'h0) begin
+        end else if (raddr2 == `RegNumLog2'h0) begin
             rdata2 <= `ZeroWord;
         end else if ((raddr2 == waddr) && (we == `WriteEnable) && (re2 == `ReadEnable)) begin
             rdata2 <= wdata;
-        end else if (re1 == `ReadEnable) begin
+        end else if (re2 == `ReadEnable) begin
             rdata2 <= regs[raddr2];
         end else begin
             rdata2 <= `ZeroWord;
